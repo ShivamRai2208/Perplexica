19ad21358382aecb83218aab362538613a96defc
ShivamRai2208 <shivam.rai@carepalsecure.com>
2025-07-10 13:55:01 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,4 +11,12 @@ const nextConfig = {
   serverExternalPackages: ['pdf-parse'],
 };
 
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn.brainplexai.com', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAA2klEQVR4AWJwL/ABNFsHFhTDQACG3wTdpKA7dJAuc1ME3gwZpkNkgoN7d1zpOxHR9K7FD059om2SZvT9AFc45FJlnnRWOLBzWw9Gpnya58ocRkDkyHaaUyXsAS20cdRo0qjR1gMKNnM0CmpzDwg3gvAuUDG8EUSL2v+JtFHQlv5A+7ADKE1ncA4Al+dWqGh2BLN9hylghUksu425gdom4B4I7gJSIEgClkCwCAiBIBxfaXEGUYz4vdSAqyO4Pg8qig4gtg7gxeEAXnpubat26Zp4PP+Ki3D4Vf8HLKicMDwr0GcAAAAASUVORK5CYII='], //
+    },
+};
+
 export default nextConfig;
