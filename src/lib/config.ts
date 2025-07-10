97ce307f92f75eb0ba515032c2d4d23ffce704d4
ShivamRai2208 <shivam.rai@carepalsecure.com>
2025-07-10 16:01:16 +0530

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
 export const getOpenaiApiKey = () => process.env.OPENAI_API_KEY || '';
 
 
-
+export const getCustomOpenaiApiKey = () => '';
+export const getCustomOpenaiApiUrl = () => '';
+export const getCustomOpenaiModelName = () => '';
 
