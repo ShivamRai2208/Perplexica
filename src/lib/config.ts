3cae755ffe675685da88aec49b9d616de8db961e
ShivamRai2208 <shivam.rai@carepalsecure.com>
2025-07-10 16:46:46 +0530

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 // src/lib/config.ts
 
-export const getOpenaiApiKey = () => process.env.OPENAI_API_KEY || '';
+export const getOpenaiApiKey = () => '';
 export const getCustomOpenaiApiKey = () => '';
 export const getCustomOpenaiApiUrl = () => '';
 export const getCustomOpenaiModelName = () => '';
-export const getGroqApiKey = () => '';
+export const getGroqApiKey = () => process.env.Groq_API_KEY || '';
 export const getAnthropicApiKey = () => '';
 export const getGeminiApiKey = () => '';
 export const getSearxngApiEndpoint = () => process.env.SEARXNG_API_URL || '';
